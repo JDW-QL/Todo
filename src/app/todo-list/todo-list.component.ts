d0db33bde818428cc2bd37aa024273d230019eaf
Jonathan Weiner <kickstandlfs@gmail.com>
2021-06-25 06:49:56 -0400

    1.0 commit

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { discardPeriodicTasks } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,31 +12,58 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./todo-list.component.css']
 })
 
-
 export class TodoListComponent {
   @Input() addTask: string = "";
   @Input() filter: string = "";
-
+  
   allComplete: boolean = false;
-
+  
   todoitem: TodoItem[] = [
     { task: "First Task", isComplete: true },
     { task: "Second Task", isComplete: true },
     { task: "Third Task", isComplete: false },
   ]
+  show = this.todoitem.map(s => false);
+  @Input() eTask = this.todoitem.map(s => "");
+  @Input() numLeft = this.calcRemainingTasks();
 
   CompleteTask(x: number) {
     this.todoitem[x].isComplete = true;
     if (this.todoitem.every(item => item.isComplete == true)) {
       this.allComplete = true;
     }
+    this.numLeft = this.calcRemainingTasks()
   }
+
+  calcRemainingTasks() {
+    return this.todoitem.filter(s => !s.isComplete).length;
+  }
+
   onDelete(index: number) {
     this.todoitem.splice(index, 1);
+    this.numLeft = this.calcRemainingTasks();
   }
-  
-  onEdit(index: number) {
 
+  EditButtonClick(index: number) {
+    if (this.show[index])
+    { 
+      this.onEdit(index);
+    }
+    else
+    {
+    this.show[index] = !this.show[index];
+    }
+  }
+
+  onEdit(index: number) {
+    if ( this.eTask[index]!="")
+    this.todoitem.splice(index, 1, 
+      {
+      task: this.eTask[index],
+      isComplete: this.todoitem[index].isComplete
+      })
+      this.eTask[index]="";
+      this.show[index] = !this.show[index];
   }
 
   AddTodo() {
@@ -48,9 +76,7 @@ export class TodoListComponent {
       });
     this.addTask = "";
     this.allComplete = false;
+    this.numLeft = this.calcRemainingTasks()
     }
   }
 }
-
-
-
